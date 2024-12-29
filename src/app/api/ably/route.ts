ddc65c54ddf3be0155b08710f55def1bfa3ebfdc
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2024-12-29 16:24:28 -0500

    puts ably route to edge runtime

--- src/app/api/ably/route.ts
+++ src/app/api/ably/route.ts
@@ -2,6 +2,10 @@ import Ably from "ably";
 
 export const revalidate = 0;
 
+export const config = {
+  runtime: 'edge'
+}
+
 export async function GET(request: Request) {
   const client = new Ably.Rest(process.env.ABLY_API_KEY as string);
   const tokenRequestData = await client.auth.createTokenRequest({
