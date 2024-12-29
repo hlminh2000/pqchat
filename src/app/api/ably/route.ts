df05aecc1aa01b6c1b5404872d3bf5a1abc5e355
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2024-12-29 09:44:47 -0500

    fixes type error

--- src/app/api/ably/route.ts
+++ src/app/api/ably/route.ts
@@ -2,8 +2,8 @@ import Ably from "ably";
 
 export const revalidate = 0;
 
-export async function GET(request) {
-  const client = new Ably.Rest(process.env.ABLY_API_KEY);
+export async function GET(request: Request) {
+  const client = new Ably.Rest(process.env.ABLY_API_KEY as string);
   const tokenRequestData = await client.auth.createTokenRequest({
     clientId: "ably-nextjs-demo",
   });
