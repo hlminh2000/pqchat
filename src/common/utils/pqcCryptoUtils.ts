c9827b098a87077d006ec1b084527efc7dda4d10
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2024-12-30 18:06:07 -0500

    use HKDF instead of PBKDF2 for key derivation

--- src/common/utils/pqcCryptoUtils.ts
+++ src/common/utils/pqcCryptoUtils.ts
@@ -5,19 +5,20 @@ export const b64ToUintArray = (b64encoded: string) => new Uint8Array(atob(b64enc
 
 export const sharedSecretToCryptoKey = async (ss: Uint8Array<ArrayBufferLike>): Promise<CryptoKey> => {
 
-  let kdk = await crypto.subtle.importKey(
+  const kdk = await crypto.subtle.importKey(
     'raw',
     ss,
     'HKDF',
     false, // KDF keys cannot be exported
-    ['deriveKey', 'deriveBits']);
+    ['deriveKey', 'deriveBits']
+  );
 
   return crypto.subtle.deriveKey(
-    { 
-      name: 'HKDF', 
-      salt: new Uint8Array(), 
-      info: new TextEncoder().encode("symetric key"), 
-      hash: 'SHA-256' 
+    {
+      name: 'HKDF',
+      salt: new Uint8Array(),
+      info: new TextEncoder().encode("symetric key"),
+      hash: 'SHA-256'
     },
     kdk,
     { name: "AES-GCM", length: 256 },
