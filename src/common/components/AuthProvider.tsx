02c4d10ee5c7588c8bffc0a101bb5953b32946c1
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2024-12-29 19:58:29 -0500

    adds uncommited file

---
+++ src/common/components/AuthProvider.tsx
@@ -0,0 +1,13 @@
+import { Auth0Provider } from "@auth0/auth0-react";
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => (
+  <Auth0Provider
+    domain="dev-48o35gs7coyf2b7q.us.auth0.com"
+    clientId="9oVYYrTOPB4nkUcCFv1AkD99UacrXKqH"
+    authorizationParams={{
+      redirect_uri: `${window.location.origin}/chat`
+    }}
+  >
+    {children}
+  </Auth0Provider>
+)
