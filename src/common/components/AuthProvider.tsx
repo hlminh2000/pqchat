1fa0e644be6edc908630ad4c622ee67a9813bd15
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2024-12-29 20:20:59 -0500

    fixes window reference problem again

--- src/common/components/AuthProvider.tsx
+++ src/common/components/AuthProvider.tsx
@@ -1,13 +1,16 @@
 import { Auth0Provider } from "@auth0/auth0-react";
 
-export const AuthProvider = ({ children }: { children: React.ReactNode }) => (
-  <Auth0Provider
-    domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN as string}
-    clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID as string}
-    authorizationParams={{
-      redirect_uri: typeof window !== "undefined" ? `${window.location.origin}/chat` : ""
-    }}
-  >
-    {children}
-  </Auth0Provider>
-)
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  if (typeof window === "undefined") return children
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN as string}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID as string}
+      authorizationParams={{
+        redirect_uri: `${window.location.origin}/chat`
+      }}
+    >
+      {children}
+    </Auth0Provider>
+  )
+}
