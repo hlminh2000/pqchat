cf75c83ff3eaa74c1d8104d4255578d25168bac3
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2024-12-30 22:39:36 -0500

    ignores type error in build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ['websocket'],
+  typescript: {
+    ignoreBuildErrors: process.env.NODE_ENV === "production", // there's an incompatibility between Next.js 15 and @auth0/nextjs-auth0
+  }
 };
 
 export default nextConfig;
