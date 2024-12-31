9d07fabd5aec4bef4ac0be320511c0d971bcca4d
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2024-12-30 22:23:39 -0500

    cannot use edge runtime for auth due to crypto :P

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,7 +1,5 @@
 import { handleAuth, handleLogout } from '@auth0/nextjs-auth0';
 
-export const runtime = "edge";
-
 export const GET = handleAuth({
   logout: handleLogout((req) => {
     return { returnTo: '/' };
