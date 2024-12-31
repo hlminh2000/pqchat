a8db9d12c73c131eb7fca1c9d2e06222c0168ea5
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2024-12-30 22:21:54 -0500

    use edge runtime for auth handling

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,5 +1,7 @@
 import { handleAuth, handleLogout } from '@auth0/nextjs-auth0';
 
+export const runtime = "edge";
+
 export const GET = handleAuth({
   logout: handleLogout((req) => {
     return { returnTo: '/' };
